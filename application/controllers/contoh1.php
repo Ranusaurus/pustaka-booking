a87490252dc4a09360fa15882621c5bdc86132ce
Ranusaurus <ranugantengsekali@gmail.com>
2022-10-07 15:18:25 +0700

    membuat model penjumlahan

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ranu IKhsan 
+            Saya tingga di daerah Solo olah raga yang saya sukai adalah Bulutangkis";
+    } 
+}
